65b9c7f531d4f95a37d485e57dbf94d7b4e03689
UyurGeerOyuncu <ulvin.oguzlu@gmail.com>
2019-02-02 18:11:13 +0400

    Butterfly Game

---
+++ butterflyGame/public_html/js/global.js
@@ -0,0 +1,12 @@
+// Code Created By UyurGezerO
+function Global() {};
+
+Global.PLAYER_ID = "player"; //Örnek
+
+Global.selectedPlayerName = "player1"; //Oyuncunun icon resmi
+Global.selectedPage = "start"; //Açık olan sayfa
+Global.playerScore = 0; //Oyuncunun skoru
+Global.playerX = 0;
+Global.playerY = 0;
+
+// Code Created By UyurGezerO
